8229dd43b07a0ca1671cb11b008453af2045b92f
Dana Maxfield <danam@synopsys.com>
2020-05-26 16:43:20 -0400

    test: PR feedback - Naming changes for AbstractExecutingSubStep (INTIJC-5)

--- src/test/java/com/synopsys/integration/stepworkflow/AbstractExecutingSubStepTest.java
+++ src/test/java/com/synopsys/integration/stepworkflow/AbstractExecutingSubStepTest.java
@@ -15,7 +15,7 @@ public class AbstractExecutingSubStepTest {
 
     private final String verificationData = "Verification output";
     private final RuntimeException verificationException = new RuntimeException("Verification runtime exception");
-    private final SubStepResponse<Object> subStepResponse_successOutput = new SubStepResponse<>(true, verificationData, verificationException);
+    private final SubStepResponse<Object> subStepResponseSuccessOutput = new SubStepResponse<>(true, verificationData, verificationException);
 
     private static Stream<Arguments> populateSuccessTests() {
         final String successData = "Success Data";
@@ -43,42 +43,42 @@ public class AbstractExecutingSubStepTest {
 
     @ParameterizedTest
     @MethodSource("populateSuccessTests")
-    public void testAbstractExecutingSubStep_Success(boolean subStepSucceeded, String data, Exception e) {
-        AbstractExecutingSubStep abstractExecutingSubStep = new AbstractExecutingSubStep_TestImpl();
-        SubStepResponse<String> subStepResponse_Input = new SubStepResponse<>(subStepSucceeded, data, e);
-        SubStepResponse<Object> subStepResponse_Output = abstractExecutingSubStep.run(subStepResponse_Input);
+    public void testAbstractConsumingSubStepSuccess(boolean subStepSucceeded, String data, Exception e) {
+        AbstractExecutingSubStep abstractExecutingSubStep = new AbstractConsumingSubStepTestImpl();
+        SubStepResponse<String> previousSubStepResponse = new SubStepResponse<>(subStepSucceeded, data, e);
+        SubStepResponse<Object> abstractConsumingSubStepResponse = abstractExecutingSubStep.run(previousSubStepResponse);
 
-        assertTrue(subStepResponse_Output.isSuccess());
-        assertFalse(subStepResponse_Output.isFailure());
-        assertTrue(subStepResponse_Output.hasData());
-        assertEquals(subStepResponse_successOutput.getData(), subStepResponse_Output.getData());
-        assertTrue(subStepResponse_Output.hasException());
-        assertEquals(subStepResponse_successOutput.getException().getMessage(), subStepResponse_Output.getException().getMessage());
+        assertTrue(abstractConsumingSubStepResponse.isSuccess());
+        assertFalse(abstractConsumingSubStepResponse.isFailure());
+        assertTrue(abstractConsumingSubStepResponse.hasData());
+        assertEquals(subStepResponseSuccessOutput.getData(), abstractConsumingSubStepResponse.getData());
+        assertTrue(abstractConsumingSubStepResponse.hasException());
+        assertEquals(subStepResponseSuccessOutput.getException().getMessage(), abstractConsumingSubStepResponse.getException().getMessage());
     }
 
     @ParameterizedTest
     @MethodSource("populateFailureTests")
-    public void testAbstractExecutingSubStep_failure(boolean subStepSucceeded, String data, Exception e) {
-        AbstractExecutingSubStep abstractExecutingSubStep = new AbstractExecutingSubStep_TestImpl();
-        SubStepResponse<String> subStepResponse_Input = new SubStepResponse<>(subStepSucceeded, data, e);
-        SubStepResponse<Object> subStepResponse_Output = abstractExecutingSubStep.run(subStepResponse_Input);
+    public void testAbstractConsumingSubStepFailure(boolean subStepSucceeded, String data, Exception e) {
+        AbstractExecutingSubStep abstractExecutingSubStep = new AbstractConsumingSubStepTestImpl();
+        SubStepResponse<String> previousSubStepResponse = new SubStepResponse<>(subStepSucceeded, data, e);
+        SubStepResponse<Object> abstractConsumingSubStepResponse = abstractExecutingSubStep.run(previousSubStepResponse);
 
-        assertFalse(subStepResponse_Output.isSuccess());
-        assertTrue(subStepResponse_Output.isFailure());
-        assertFalse(subStepResponse_Output.hasData());
-        assertNull(subStepResponse_Output.getData());
-        assertEquals(subStepResponse_Input.hasException(), subStepResponse_Output.hasException());
+        assertFalse(abstractConsumingSubStepResponse.isSuccess());
+        assertTrue(abstractConsumingSubStepResponse.isFailure());
+        assertFalse(abstractConsumingSubStepResponse.hasData());
+        assertNull(abstractConsumingSubStepResponse.getData());
+        assertEquals(previousSubStepResponse.hasException(), abstractConsumingSubStepResponse.hasException());
 
-        if (subStepResponse_Output.hasException()) {
-            assertEquals(subStepResponse_Input.getException().getMessage(), subStepResponse_Output.getException().getMessage());
+        if (abstractConsumingSubStepResponse.hasException()) {
+            assertEquals(previousSubStepResponse.getException().getMessage(), abstractConsumingSubStepResponse.getException().getMessage());
         }
     }
 
-    public class AbstractExecutingSubStep_TestImpl extends AbstractExecutingSubStep {
+    public class AbstractConsumingSubStepTestImpl extends AbstractExecutingSubStep {
 
         @Override
         public SubStepResponse<Object> run() {
-            return subStepResponse_successOutput;
+            return subStepResponseSuccessOutput;
         }
     }
 
